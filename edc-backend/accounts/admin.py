886af912ca3803025acf8fba4d28aeefcc15814e
HARIHARAN VIJAYAN <720hariharanoffl@gmail.com>
2026-09-20 19:16:11 +0530

    Deleted faulty backend directory

--- edc-backend/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-
-@admin.register(User)
-class EDCUserAdmin(UserAdmin):
-    list_display = ("email", "first_name", "last_name", "role", "is_staff")
-    fieldsets = UserAdmin.fieldsets + (("Role", {"fields": ("role",)}),)
--- edc-backend/events/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import EventViewSet, RegistrationViewSet
-
-router = DefaultRouter()
-router.register("events", EventViewSet, basename="event")
-router.register("registrations", RegistrationViewSet, basename="registration")
-
-urlpatterns = router.urls
